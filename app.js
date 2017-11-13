24a3fb9172ca0bf13e451f6f13f397ea4996e59d
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-11-13 13:58:54 -0800

    first trial

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 'use strict'
+var dataEntry = (
+  location: '1st And Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki',
+  minCustomers: 23, 3, 11, 20, 2,
+  maxCustomers: 65, 24, 38, 38, 16,
+  avgCookieSale: 6.3, 1.2, 3.7, 2.3, 4.6,
+);
+
+var firstAndPike = (
+  minCust: 23,
+  maxCustomers: 65,
+  averageCookieSale: 6.3,
+);
+
+var seaTacAirport = (
+  minCustomers: 3,
+  maxCookies: 24,
+  avgCookieSale: 1.2,
+);
+
+var seattleCenter = (
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookieSale: 3.7,
+);
+
+var capitolHill = (
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookieSale: 2.3,
+);
+
+var alki = (
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookieSale: 4.6,
+);
